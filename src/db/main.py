5456a67439b7507ce0c77817d2835d4476684767
Asogwa Chukwuemeka <101509899+Mekazstan@users.noreply.github.com>
2025-03-08 22:14:39 +0100

    Update main.py

--- src/db/main.py
+++ src/db/main.py
@@ -4,25 +4,27 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import sessionmaker
 from src.config import Config
 
-
 async_engine = AsyncEngine(
     create_engine(
         url=Config.DATABASE_URL,
-        echo=True
+        echo=True,
+        pool_size=20,
+        max_overflow=10,
+        pool_recycle=3600,
+        pool_pre_ping=True,
     )
 )
 
 async def init_db():
     async with async_engine.begin() as conn:
-        
         await conn.run_sync(SQLModel.metadata.create_all)
-        
+
 async def get_session() -> AsyncSession:
     Session = sessionmaker(
-        bind = async_engine,
+        bind=async_engine,
         class_=AsyncSession,
-        expire_on_commit=False
+        expire_on_commit=False,
     )
-    
+
     async with Session() as session:
         yield session
